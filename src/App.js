6172dce243a181179979dcd3fb5948372b2f913f
pranesh <praneshsenthilkumar18@gmail.com>
2024-10-18 19:18:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import QuotesApp from './components/quotesapp';
+function App() {
+  return (
+    <div className="App">
+      <QuotesApp />
+    </div>
+  );
+}
+
+export default App;
